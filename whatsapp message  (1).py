a347670913853e4a5395c38faf265f56264417d9
Nishant dixit <145234347+bleedblack1@users.noreply.github.com>
2023-09-21 22:32:36 +0530

    Add files via upload

---
+++ whatsapp message  (1).py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pywhatkit
+
+
+# In[3]:
+
+
+pywhatkit.sendwhatmsg_to_group_instantly("Igm4MjDccoLAxrHxzjkm1P","hello")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
